e5516988d4ae1460e89e3837e9c6d3a222c6afca
Iuri Almeida <ialmeida@vertigo.com.br>
2022-01-26 18:28:14 -0300

    Melhorando regra para acessar a equipe

--- src/main/java/br/com/ialmeida/controller/RegisterController.java
+++ src/main/java/br/com/ialmeida/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package br.com.ialmeida.controller;
 
 import br.com.ialmeida.entities.Rebel;
+import br.com.ialmeida.enums.Race;
 import lombok.Cleanup;
 import lombok.NoArgsConstructor;
 
@@ -30,6 +31,8 @@ public class RegisterController {
     }
 
     public boolean requestAccess(Rebel rebel) {
-        return rebel.getAge() > 20 && rebel.getAge() < 50;
+        return (rebel.getAge() > 20 && rebel.getAge() < 50 && rebel.getRace() == Race.HUMAN) ||
+                (rebel.getAge() > 30 && rebel.getAge() < 70 && rebel.getRace() == Race.GREE) ||
+                (rebel.getAge() > 0 && rebel.getAge() < 20 && rebel.getRace() == Race.RAKATA);
     }
 }
